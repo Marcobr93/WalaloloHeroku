31b6466e35c3bbbbe8a18200b6beb66658ed37c1
Marcobr93 <marcoabr93@gmail.com>
2018-06-19 17:31:22 +0200

    scss

--- public/js/datePicker.js
+++ public/js/datePicker.js
@@ -60,20 +60,20 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 56);
+/******/ 	return __webpack_require__(__webpack_require__.s = 52);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 56:
+/***/ 52:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(57);
+module.exports = __webpack_require__(53);
 
 
 /***/ }),
 
-/***/ 57:
+/***/ 53:
 /***/ (function(module, exports) {
 
 $(function () {
